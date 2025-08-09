cc6904bcff1fa10be33e32b2424aae2045378778
Merciless <yurii.afterglow@gmail.com>
2025-08-09 11:23:47 +0300

    feat: add /users/check-email endpoint

--- backend/src/modules/users/users.controller.ts
+++ backend/src/modules/users/users.controller.ts
@@ -2,6 +2,7 @@ import { UsersService, usersService } from './users.service';
 import { ContextWithIdParam, TypedContext } from '../../common/types/koa';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { CheckEmailDto } from './dto/check-email.dto';
 
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
@@ -42,6 +43,13 @@ export class UsersController {
     await this.usersService.delete(userId);
 
     ctx.status = 204;
+  }
+
+  async checkEmail(ctx: TypedContext<CheckEmailDto>) {
+    const { email } = ctx.request.body;
+    const user = await this.usersService.findByEmail(email);
+
+    ctx.body = { exists: !!user };
   }
 }
 
