e07772f4edd3392eac36bb4e21bcbb14add6d84c
Merciless <yurii.afterglow@gmail.com>
2025-08-11 03:47:17 +0300

    feat(tabs): add tab buttons

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,28 +1,38 @@
 import { Routes } from '@angular/router';
+import { TabsPage } from './tabs/tabs.page';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () =>
-      import('./pages/home/home.page').then((m) => m.HomePage),
-  },
-  {
-    path: 'auth',
-    loadComponent: () =>
-      import('./pages/auth/auth.page').then((m) => m.AuthPage),
-  },
-  {
-    path: 'timer',
-    loadComponent: () =>
-      import('./pages/timer/timer.page').then((m) => m.TimerPage),
-  },
-  {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    component: TabsPage,
+    children: [
+      {
+        path: 'home',
+        loadComponent: () =>
+          import('./pages/home/home.page').then((m) => m.HomePage),
+      },
+      {
+        path: 'auth',
+        loadComponent: () =>
+          import('./pages/auth/auth.page').then((m) => m.AuthPage),
+      },
+      {
+        path: 'timer',
+        loadComponent: () =>
+          import('./pages/timer/timer.page').then((m) => m.TimerPage),
+      },
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+    ],
   },
   {
     path: 'timer-execution',
-    loadComponent: () => import('./pages/timer-execution/timer-execution.page').then( m => m.TimerExecutionPage)
+    loadComponent: () =>
+      import('./pages/timer-execution/timer-execution.page').then(
+        (m) => m.TimerExecutionPage,
+      ),
   },
 ];
---
+++ frontend/src/app/tabs/tabs.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-tabs',
+  imports: [IonicModule, RouterModule],
+  templateUrl: 'tabs.page.html',
+})
+export class TabsPage {}
